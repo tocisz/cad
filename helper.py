3d2fe57235fc81168351846bf886d6c7bbbbc79a
Tomasz Cichocki <cichymail@gmail.com>
2025-02-02 12:28:33 +0100

    base for a spool

---
+++ helper.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+def fix(s, es):
+    for e in es:
+        s = s.constrain(e, "Fixed", None)
+    return s
+
+def npoint(p):
+    """
+    Convert a tuple (x, y) into homogeneous coordinates [x, y, 1].
+    """
+    x, y = p
+    return np.array([x, y, 1])
+
+def point_tuple(p):
+    """
+    Convert a homogeneous coordinate [x, y, w] back to a tuple (x, y).
+    """
+    return (p[0] / p[2], p[1] / p[2])
+
+def move(p,d):
+    x,y = p
+    dx,dy = d
+    return (x+dx, y+dy)
+
+def line_from_points(p1, p2):
+    """
+    Compute the line representation l = [a, b, c] given two points p1 and p2.
+    """
+    # Cross product to get line equation coefficients
+    return np.cross(p1, p2)
+
+def intersection_of_lines(l1, l2):
+    """
+    Compute the intersection point of two lines l1 and l2.
+    """
+    # Cross product to get intersection point
+    p = np.cross(l1, l2)
+    
+    # Normalize if the third coordinate is not 1 (to keep it in homogeneous coordinates)
+    if p[2] != 0:
+        p = p / p[2]
+    
+    return p
+
+def vertical_line(x1):
+    """
+    Create a vertical line x = x1 in homogeneous representation [1, 0, -x1].
+    """
+    return np.array([1, 0, -x1])
+
+def horizontal_line(y1):
+    """
+    Create a horizontal line y = y1 in homogeneous representation [0, 1, -y1].
+    """
+    return np.array([0, 1, -y1])
+
+# p1 = npoint((1,1))
+# p2 = npoint((3,2))
+
+# l1 = line_from_points(p1, p2)
+# print(l1)
+
+# print(np.dot(l1, p1)) # should be close to 0
+# print(np.dot(l1, p2)) # should be close to 0
+
+# p3 = intersection_of_lines(l1, vertical_line(2))
+# print(point_tuple(p3)) # should be (2,1.5)
--- spindle.py
+++ spindle.py
@@ -3,6 +3,7 @@
 import cadquery as cq
 from ocp_vscode import *
 import numpy as np
+from helper import *
 # %%
 
 # Define widths
@@ -27,42 +28,88 @@ l = np.array([
 ls = np.cumsum(l)
 ls = np.append(ls, 51) # total length
 
-# Define constuction segments
-e = np.array([
-    [(0, 0), (0, ls[2])], # bottom left
-    [(r[0], 0), (r[2], ls[2])], # bottom right
-    [(0, ls[4]), (r[4], ls[4])], # spike base
-    [(0, ls[5]), (r[5], ls[5])], # spike top
-    [(0, ls[2]), (r[3], ls[2])], # spool base bottom
-    [(r[3], ls[2]), (r[3], ls[3])], # spool right
-])
-
 notch_point = (l[0] + l[1]/2) / ls[2]
 fr = 0.5 # default fillet radius
-
+#%%
 s = cq.Sketch()
-for i in range(e.shape[0]):
-    s = s.segment(tuple(e[i][0]), tuple(e[i][1]), f"e{i+1}", True)
+# constuction segments
+s = s.segment((0, 0),        (0, ls[2]),    "e1", True) # bottom left
+s = s.segment((r[0], 0),     (r[2], ls[2]), "e2", True) # bottom right
+s = s.segment((0, ls[4]),    (r[4], ls[4]), "e3", True) # spike base
+s = s.segment((0, ls[5]),    (r[5], ls[5]), "e4", True) # spike top
+s = s.segment((0, ls[2]),    (r[3], ls[2]), "e5", True) # spool base bottom
+s = s.segment((r[3], ls[2]), (r[3], ls[3]), "e6", True) # spool right
 s = s.segment((0, l[0]), (1, l[0]), "middle", True)
-for i in range(e.shape[0]):
-    s = s.constrain(f"e{i+1}", "Fixed", None)
-s = s.constrain("middle", "Orientation", (1,0))
+s = fix(s, [f"e{i}" for i in range(1,7)])
 s = s.constrain("middle", "e1", "Distance", (0.0, notch_point, 0.0))
 s = s.constrain("middle", "e2", "Distance", (1.0, notch_point, 0.0))
 s = s.solve()
 s = s.select("e1", "e2").hull()
 s = s.select("e3", "e4").hull()
 s = s.reset().vertices(">(1,1,0)").fillet(r[5])
-s = s.reset().vertices(">X", tag="middle").rect(w[1], l[1], mode="s")
+s = s.reset().vertices(">X", tag="middle").rect(2*w[1], l[1], mode="s")
 s = s.select("e3", "e5", "e6").hull(tag="base")
 s = s.reset().vertices(">(1,1,0)", tag="base").fillet(fr)
 s = s.clean()
 s = s.reset().vertices("<<X[2]").fillet(fr)
 # show(s)
+# show(s, position=(0, 0, 90), target=(0, 25, 0))
 
-body = cq.Workplane("XZ").placeSketch(s).revolve()
-show(body)
+body1 = cq.Workplane("XZ").placeSketch(s).revolve()
+# show(body1)
 
 #%%
-cq.exporters.export(body, "spindle.stl")
-cq.exporters.export(body, "spindle.step")
+r0 = 8/2
+r1 = 46/2
+r2 = 54/2
+h = 8
+thick = 2.3
+ttop = 1
+t2 = 1
+distance = 0.2
+fr1 = 0.4
+fr2 = 0.4
+fr3 = 1
+
+e_ref = (r[4], ls[4]), (r[5], ls[5])
+e_ref1 = tuple(map(lambda p: move(p, (distance, 0)), e_ref))
+
+s = cq.Sketch()
+# s = s.segment(*e_ref, "ex", True)
+p1 = (r2, ls[4]+h)
+p2 = (r1+t2, ls[4])
+s = s.segment(p1, p2, "outer")
+s = s.segment(p2, e_ref1[0], "bottom") # base bottom
+l1 = horizontal_line(ls[4]+h)
+p1 = point_tuple(intersection_of_lines(line_from_points(*map(npoint, e_ref1)),l1))
+s = s.segment(e_ref1[0], p1, "inner")
+p3 = move(p1,(ttop,0))
+s = s.segment(p1, p3, "h1") # inner rim
+p4 = (r0, ls[4]+thick)
+s = s.segment(p3, p4, "s1") # slope down
+p5 = (r1, ls[4]+thick)
+s = s.segment(p4, p5, "b1") # upper bottom
+p6 = (r2-ttop, ls[4]+h)
+s = s.segment(p5, p6, "s1") # slope up
+s = s.close()
+s = s.assemble()
+s = s.reset().vertices(">Y").fillet(fr1)
+s = s.reset().vertices(">>Y[1]").fillet(fr2)
+s = s.reset().vertices("<Y").chamfer(fr3)
+s = s.reset().vertices(">>Y[1]").fillet(2*fr3)
+s = s.reset().vertices(">>Y[2]").fillet(2*fr3)
+
+# show(s, position=(0, 0, 90), target=(0, 25, 0))
+
+# %%
+body2 = cq.Workplane("XZ").placeSketch(s).revolve()
+
+# %%
+
+show(body1, body2)
+
+#%%
+cq.exporters.export(body1, "spindle1.stl")
+cq.exporters.export(body1, "spindle1.step")
+cq.exporters.export(body2, "spindle2.stl")
+cq.exporters.export(body2, "spindle2.step")
