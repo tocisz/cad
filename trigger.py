c6e41b3a39bc8784a1dc9ca04fbcddfd8a3f87cb
tocisz <cichymail@gmail.com>
2025-03-31 21:55:05 +0200

    trigger for passap row trip counter
    not whole, just part of it to glue it

---
+++ trigger.py
@@ -0,0 +1,24 @@
+#%%
+from build123d import *
+from ocp_vscode import *
+import math
+#%%
+w = 64*MM
+r = 20*MM
+h = 5*MM
+d = 2*MM
+angle = math.degrees(math.atan2(h, w/2))
+
+with BuildSketch() as outline:
+    Triangle(a=w, B=angle, C=angle, align=(Align.CENTER,Align.MIN))
+    fillet(outline.vertices().sort_by(Axis.Y)[-1], radius=r)
+
+show(outline.sketch)
+
+# %%
+with BuildPart() as part:
+    add(outline)
+    extrude(amount=d)
+show(part.part)
+# %%
+export_step(part.part, "trigger.step")
