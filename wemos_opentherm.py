1691321dd560a9930561df37d0af2f0aa795e70d
Tomasz Cichocki <cichymail@gmail.com>
2025-08-26 10:42:55 +0200

    use sketches to define walls around the board

--- wemos_opentherm.py
+++ wemos_opentherm.py
@@ -21,27 +21,19 @@ def find_by_label(shape: Compound, target: str):
 d1_main = find_by_label(d1_step, "COMPOUND")[0]
 d1_main.label = "D1 Main"
 d1_main.color = Color(0.8, 0.8, 0.8)
-d1_bbox = d1_main.bounding_box()
-d1_size = d1_bbox.size
 
 d1_usb = find_by_label(d1_step, "USB")[0]
 d1_usb.label = "USB Port"
 d1_usb.color = Color(0.8, 0.8, 0.2)
-d1_usb_bbox = d1_usb.bounding_box()
-d1_usb_size = d1_usb_bbox.size
-
-d1_usb_offset = d1_usb_bbox.center() - d1_bbox.center()
 
 d1_switch = find_by_label(d1_step, "Switch")[0]
 d1_switch.label = "Switch"
 d1_switch.color = Color(0.8, 0.2, 0.2)
-d1_switch_bbox = d1_switch.bounding_box()
-d1_switch_size = d1_switch_bbox.size
 
 #%% Screw mock
 screw_diameter = 2.5
-screw_length = 8
 with BuildPart() as screw_mock:
+    screw_length = 8
     with BuildSketch(Plane.XY):
         Circle(screw_diameter/2)
     extrude(amount=screw_length)
@@ -63,9 +55,7 @@ with BuildPart() as to92_mock:
 show(to92_mock.part, sk.sketch_local)
 
 #%% Upper hat board mock
-d1_switch_offset = d1_switch_bbox.center() - d1_bbox.center()
-
-d1_full = Compound(children=[d1_main, d1_usb, d1_switch], label="d1_full")
+d1_full = Compound(children=[d1_main, d1_usb, d1_switch], label="Wemos D1")
 
 d1_pcb_measured_thinkness = 1.4
 hat_thickness = 2.0
@@ -76,7 +66,7 @@ screw_pos_side = 10
 
 with BuildPart() as hat_board:
     with BuildSketch(Plane.XY.offset(hat_distance_from_d1)):
-        Rectangle(d1_bbox.size.X, d1_bbox.size.Y)
+        Rectangle(d1_main.bounding_box().size.X, d1_main.bounding_box().size.Y)
     extrude(amount=hat_thickness)
 
     hat_bbox = hat_board.part.bounding_box()
@@ -131,7 +121,7 @@ with BuildPart() as latch:
     RigidJoint(label="latch_joint", joint_location=joint)
 
 show(latch.part)
-#%% Lower part of the housing
+#%% Sketches for lower part of the housing
 bbox = d1_full.bounding_box()
 
 clearance = 0.4
@@ -139,64 +129,55 @@ wall_thickness = 1.2
 extra_height = 2.5  # mm above board
 bottom_space = 3  # mm below usb
 
-# Outer box
-outer_size = (
-    bbox.size.X + 2 * (clearance + wall_thickness),
-    bbox.size.Y + 2 * (clearance + wall_thickness),
-    bbox.size.Z + extra_height + wall_thickness + bottom_space,  # includes bottom wall
-)
-
-outer_pos = (
-    bbox.min.X - (clearance + wall_thickness),
-    bbox.min.Y - (clearance + wall_thickness),
-    bbox.min.Z - wall_thickness - bottom_space,
-)
-
 fin_length = 25
 fin_width = 1.2
-fin_height = d1_bbox.min.Z - d1_usb_bbox.min.Z + bottom_space
-fin_position_x = d1_size.X / 2 - 3.5
-fin_position_y = d1_size.Y / 2 + clearance
+fin_height = d1_main.bounding_box().min.Z - d1_usb.bounding_box().min.Z + bottom_space
+fin_position_x = d1_main.bounding_box().size.X / 2 - 3.5
+fin_position_y = d1_main.bounding_box().size.Y / 2 + clearance
 
 usb_cutout_width = 10.8
 usb_cutout_height = 8
 
+bottom_plane = Plane.XY.offset(bbox.min.Z - bottom_space)
+middle_plane = bottom_plane.offset(fin_height)
+with BuildSketch(bottom_plane) as hat_bottom_inner:
+    Rectangle(bbox.size.X, bbox.size.Y)
+    offset(amount=clearance)
+with BuildSketch(bottom_plane) as hat_bottom_outer:
+    Rectangle(bbox.size.X, bbox.size.Y)
+    offset(amount=clearance+wall_thickness)
+with BuildSketch(bottom_plane) as hat_bottom:
+    add(hat_bottom_outer)
+    add(hat_bottom_inner, mode=Mode.SUBTRACT)
+    with Locations([(fin_position_x, -fin_position_y), (-fin_position_x, -fin_position_y)]):
+        r = Rectangle(fin_width, fin_length, align=(Align.CENTER, Align.MIN))
+        fillet(r.vertices(), clearance)
+with BuildSketch(middle_plane) as hat_middle:
+    add(hat_bottom_outer)
+    add(hat_bottom_inner, mode=Mode.SUBTRACT)
+
+show(d1_full, hat_bottom.sketch, hat_middle.sketch)
+
+#%% Lower part of the housing
 with BuildPart() as housing:
-    with BuildSketch(Plane.XY.offset(outer_pos[2])):
-        Rectangle(outer_size[0], outer_size[1], align=(Align.CENTER, Align.CENTER))
-    extrude(amount=outer_size[2])
-
-    # Pick the top face
-    top_face = housing.faces().sort_by(Axis.Z)[-1]
-    bottom_face = housing.faces().sort_by(Axis.Z)[0]
-    back_face = housing.faces().sort_by(Axis.Y)[-1]
-    right_face = housing.faces().sort_by(Axis.X)[-1]
-
-    # Hollow it out with open top
-    offset(amount=-wall_thickness, openings=[top_face])
-
-    # Add fins
-    with BuildSketch(Plane(bottom_face).reverse().offset(wall_thickness)) as fins_sketch:
-        with Locations([(fin_position_x, -fin_position_y), (-fin_position_x, -fin_position_y)]):
-            Rectangle(fin_width, fin_length, align=(Align.CENTER, Align.MIN))
-    extrude(amount=fin_height, mode=Mode.ADD)
-
-    # Fillet vertical edges
-    fillet(housing.edges().filter_by(Axis.Z), 0.4)
-
-    back_face_z_shift = back_face.bounding_box().size.Z/2
+    extrude(hat_bottom_outer.sketch, amount=-wall_thickness)
+    extrude(hat_bottom.sketch, amount=fin_height)
+    extrude(hat_middle.sketch, amount=d1_main.bounding_box().max.Z-middle_plane.location.position.Z+extra_height)
 
     # Add cutout for USB
+    back_face = housing.faces().sort_by(Axis.Y)[-1]
+    back_face_z_shift = back_face.bounding_box().size.Z/2
     with BuildSketch(Plane(back_face).reverse().move(Location((0,0,wall_thickness-back_face_z_shift)))):
         r = Rectangle(usb_cutout_width, usb_cutout_height, align=(Align.CENTER, Align.MIN))
         fillet(r.vertices(), 1)
     extrude(amount=wall_thickness, mode=Mode.SUBTRACT)
 
     # Add cutout for switch
-    switch_cutout_width = d1_switch_size.Y
-    switch_cutout_height = d1_switch_size.Z
+    right_face = housing.faces().sort_by(Axis.X)[-1]
+    switch_cutout_width = d1_switch.bounding_box().size.Y
+    switch_cutout_height = d1_switch.bounding_box().size.Z
     switch_cutout_z_shift = 0.95 # why it's needed?
-    with BuildSketch(Plane(right_face).reverse().move(Location((0, d1_switch_bbox.center().Y, d1_switch_bbox.center().Z+switch_cutout_z_shift)))):
+    with BuildSketch(Plane(right_face).reverse().move(Location((0, d1_switch.bounding_box().center().Y, d1_switch.bounding_box().center().Z+switch_cutout_z_shift)))):
         r = Rectangle(switch_cutout_width, switch_cutout_height)
         fillet(r.vertices(), 1)
     extrude(amount=wall_thickness, mode=Mode.SUBTRACT)
@@ -239,8 +220,8 @@ with BuildSketch(sketch_plane) as cutouts:
         Circle(screw_diameter/2+clearance)
 
 with BuildSketch(sketch_plane) as upper_housing_outline:
-    r1 = Rectangle(d1_bbox.size.X + 2*(clearance + wall_thickness),
-              d1_bbox.size.Y + 2*(clearance + wall_thickness))
+    r1 = Rectangle(d1_main.bounding_box().size.X + 2*(clearance + wall_thickness),
+                   d1_main.bounding_box().size.Y + 2*(clearance + wall_thickness))
     r1_corner = r1.vertices().group_by(Axis.X)[-1].sort_by(Axis.Y)[-1]
     corner_cut_x = r1_corner.X - bs18b20.bounding_box().min.X + clearance
     corner_cut_y = r1_corner.Y - bs18b20.bounding_box().min.Y + clearance
