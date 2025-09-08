5250d3831589224637e3fe0bdc74d51a29fa47a9
Tomasz Cichocki <cichymail@gmail.com>
2025-09-08 18:22:37 +0200

    on sketches

--- letters.py
+++ letters.py
@@ -1,76 +1,96 @@
 #%% Imports
+from math import sqrt
 from build123d import *
 from ocp_vscode import show
-from math import radians, tan
-import copy
 #%% a
-la = Line((0, 1), (1, 0))
-la
-#%%
-ls = Line((0, 0), (1, 1))
-ls
-#%%
-lm = RadiusArc((0, 0), (1, 1), 1)
-lm
-#%%
-lo = RadiusArc((0, 1), (1, 0), 1)
-lo
-#%%
-lt = RadiusArc((0, 0), (1, 1), -1)
-lt
-#%%
-li = RadiusArc((0, 1), (1, 0), -1)
-li
-#%%
-with BuildLine() as lkb:
-    TangentArc([(0,0), lm @ 0.5], tangent=(1,0))
-    TangentArc([lm @ 0.5, (1,1)], tangent=(1,-1))
-lk = lkb.wire()
-lk
-#%%
-with BuildLine() as lwb:
-    TangentArc([(0,0), lt @ 0.5], tangent=(0,1))
-    TangentArc([lt @ 0.5, (1,1)], tangent=(-1,1))
-lw = lwb.wire()
-lw
-#%%
-with BuildLine() as lyb:
-    TangentArc([(0,1), li @ 0.5], tangent=(1,0))
-    TangentArc([li @ 0.5, (1,0)], tangent=(1,1))
-ly = lyb.wire()
-ly
-#%%
 cx = Location((0.5,0.5))
-with BuildLine() as llb:
-    add(lt)
-    r = (cx.position - (lt @ 0.5)).length
-    add(Wire.make_circle(r).moved(cx))
-ll = Compound(llb.wires())
-ll
+LW = 0.05 # line width
+with BuildSketch() as ssb:
+    with Locations(cx):
+        SlotCenterToCenter(sqrt(2), 2*LW, 45)
+show(ssb.sketch)
 #%%
-with BuildLine() as leb:
-    add(li)
-    r = (cx.position - (li @ 0.5)).length
-    add(Wire.make_circle(r).moved(cx))
-le = Compound(leb.wires())
-le
+mirror_x_plane = Plane.XZ.move(cx)
+mirror_xy_plane = Plane.XZ.move(Location(cx.position, 45))
+with BuildSketch() as sab:
+    add(ssb.sketch.mirror(mirror_x_plane))
+show(sab.sketch)
 #%%
-with BuildLine() as lpb:
-    add(lm)
-    r = (cx.position - (lm @ 0.5)).length
-    add(Wire.make_circle(r).moved(cx))
-lp = Compound(lpb.wires())
-lp
+lmw = RadiusArc((0, 0), (1, 1), 1)
+with BuildSketch() as smb:
+    with BuildLine():
+        add(lmw)
+        # RadiusArc((0, 0), (1, 1), 1)
+    make_face()
+    offset(amount=LW)
+show(smb.sketch)
+#%%
+with BuildSketch() as sib:
+    add(smb.sketch.mirror(mirror_x_plane))
+show(sib.sketch)
+#%%
+with BuildSketch() as stb:
+    add(smb.sketch.mirror(mirror_xy_plane))
+show(stb.sketch)
+#%%
+with BuildSketch() as sob:
+    add(stb.sketch.mirror(mirror_x_plane))
+show(sob.sketch)
+#%%
+with BuildSketch() as skb:
+    with BuildLine():
+        TangentArc([(0,0), lmw @ 0.5], tangent=(1,0))
+        TangentArc([lmw @ 0.5, (1,1)], tangent=(1,-1))
+    make_face()
+    offset(amount=LW)
+show(skb.sketch)
+#%%
+with BuildSketch() as swb:
+    add(skb.sketch.mirror(mirror_xy_plane))
+show(swb.sketch)
+#%%
+with BuildSketch() as syb:
+    add(skb.sketch.mirror(mirror_x_plane))
+show(syb.sketch)
+#%%
+with BuildSketch() as spb:
+    with BuildLine() as lpb:
+        r = (cx.position - (lmw @ 0.5)).length
+        add(Wire.make_circle(r).moved(cx))
+    f = make_face()
+    offset(amount=LW)
+    with BuildSketch() as inside:
+        add(f)
+        offset(amount=-LW)
+    add(inside.sketch, mode=Mode.SUBTRACT)
+    add(smb.sketch)
+show(spb.sketch)
+#%%
+with BuildSketch() as slb:
+    add(spb.sketch.mirror(mirror_xy_plane))
+show(slb.sketch)
+#%%
+with BuildSketch() as seb:
+    add(spb.sketch.mirror(mirror_x_plane))
+show(seb.sketch)
 #%%
 # Based on circle: center on horizontal line y = 0.5, coincident with cx, and M arc
-with BuildLine() as lrb:
-    k1 = Vector(0.2, 0.6)
-    k2 = Vector(0.8, 0.4)
-    a1 = TangentArc([(0,0),k1], tangent=(0,1))
-    a2 = TangentArc([a1@1, cx.position], tangent=a1%1)
-    a3 = TangentArc([a2@1, k2], tangent=a2%1)
-    TangentArc([a3@1, (1,1)], tangent=a3%1)
-lr = Compound(lrb.wires())
-lr
-
+with BuildSketch() as srb:
+    with BuildLine():
+        k1 = Vector(0.2, 0.6)
+        k2 = Vector(0.8, 0.4)
+        a1 = TangentArc([(0,0),k1], tangent=(0,1))
+        a2 = TangentArc([a1@1, cx.position], tangent=a1%1)
+        a3 = TangentArc([a2@1, k2], tangent=a2%1)
+        TangentArc([a3@1, (1,1)], tangent=a3%1)
+    make_face()
+    offset(amount=LW)
+show(srb.sketch)
 # %%
+with BuildSketch() as sub:
+    add(srb.sketch.mirror(mirror_x_plane))
+show(sub.sketch)
+# %%
+with BuildSketch() as scb:
+    add(srb.sketch.mirror(mirror_xy_plane))
+show(scb.sketch)
